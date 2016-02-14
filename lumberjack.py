86400bc3a9578483d58c2371ffccb053d91ee213
mrfr0st <dmoroz73@gmail.com>
2016-02-14 09:42:30 +0000

    Initial commit

---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Dmitriy": 
+    print("{} is a rockstar and he's OK!".format(name))
+else:
+    print("{} sleeps all night and {} works all day!".format(name, name))
